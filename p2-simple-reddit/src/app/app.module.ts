f1df57ef4621075fe2562bc5e1dd09180672c283
Elroy Kanye <elroykanye@gmail.com>
2022-03-20 19:56:48 +0100

    mod app to display multiple articles

--- p2-simple-reddit/src/app/app.module.ts
+++ p2-simple-reddit/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {SuiModule} from "ng2-semantic-ui";
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
