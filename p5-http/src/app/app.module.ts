96f33bb231fcf5c1c3c60aed93e6bec77ed37254
Elroy Kanye <elroykanye@gmail.com>
2022-04-26 12:16:36 +0100

    semig complete http

--- p5-http/src/app/app.module.ts
+++ p5-http/src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { SimpleHttpComponent } from './component/simple-http/simple-http.component';
+import {HttpClientModule} from "@angular/common/http";
+import {youtubeSearchInjectable} from "./injectable/youtube-search.injectable";
+import { SearchBoxComponent } from './component/search-box/search-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    SearchBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    youtubeSearchInjectable
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
